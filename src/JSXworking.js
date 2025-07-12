0babb35b488c56cbccda9ecc0d557486f3b91341
amirtuyu <amirtuyu@gmail.com>
2025-07-12 14:19:23 +0330

    Project completion

--- src/JSXworking.js
+++ src/JSXworking.js
@@ -1,4 +1,6 @@
 import "./JSXworking.css";
+import { motion, AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
 function DeletItem(props) {
   return (
     <span
@@ -10,22 +12,102 @@ function DeletItem(props) {
   );
 }
 function OpenAddText(props) {
-  return <span onClick={props.handlerOpenAndCloseTextarea} className="material-symbols-outlined addtext">add</span>;
+  return (
+    <span
+      onClick={props.handlerOpenAndCloseTextarea}
+      className="material-symbols-outlined addtext"
+    >
+      add
+    </span>
+  );
 }
-function CloseAddText(props){
-return<span onClick={props.handlerOpenAndCloseTextarea} className="material-symbols-outlined closeAddTextv ">
-close
-</span>
+function CloseAddText(props) {
+  return (
+    <span
+      onClick={props.handlerOpenAndCloseTextarea}
+      className="material-symbols-outlined closeAddTextv "
+    >
+      close
+    </span>
+  );
 }
-function TextArea(props){
-  return(
+function TextArea(props) {
+  return (
     <>
-  <textarea id={props.id} onChange={props.getText} className="textarea" placeholder ="Title..."></textarea>
-   <button onClick={()=>{props.handlerText();props.updateRipository()}} className="buttonChangText">ChangText</button>
-   </>
-  )
+      <textarea
+        value={props.value}
+        id={props.id}
+        onChange={(event) => {
+          props.getText(event);
+          props.onchangForEdite(event);
+        }}
+        className="textarea"
+        placeholder="Title..."
+      ></textarea>
+      <button
+        onClick={() => {
+          props.handlerText();
+          props.updateRipository();
+        }}
+        className="buttonChangText"
+      >
+        ChangText
+      </button>
+    </>
+  );
 }
-function OpenAndCloseText(){
-  return <span class="material-symbols-outlined Icon-animation-Text">chevron_right</span>
+function OpenAndCloseText(props) {
+  let classs = `material-symbols-outlined Icon-animation-Text ${
+    props.openAndCloseText && "toggle-icon"
+  }`;
+  return (
+    <span className={classs} onClick={props.handlerOpenAndCloseText}>
+      chevron_right
+    </span>
+  );
 }
-export { DeletItem, OpenAddText,OpenAndCloseText,CloseAddText, TextArea};
+function Text(props) {
+  const [isOpen, setIsOpen] = useState(true);
+  useEffect(() => {
+    setIsOpen(props.openAndCloseText);
+  }, [props.openAndCloseText]);
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="Text"
+          initial={{ height: 0, opacity: 0 }}
+          animate={{ height: "auto", opacity: 1 }}
+          exit={{ height: 0, opacity: 0 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          style={{
+            overflow: "hidden",
+          }}
+        >
+          <div className="containerIconEditAndTimer">
+            <div className="Timer">0seconds</div>
+            <span
+              onClick={() => {
+                props.getTextForEdite();
+                props.updateRipository();
+              }}
+              className="material-symbols-outlined show-icon"
+              style={{ cursor: "pointer" }}
+            >
+              edit
+            </span>
+          </div>
+          <div className="containerTargetText">{props.text}</div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
+export {
+  DeletItem,
+  OpenAddText,
+  OpenAndCloseText,
+  CloseAddText,
+  TextArea,
+  Text,
+};
--- src/List.js
+++ src/List.js
@@ -1,17 +1,20 @@
 import "./List.css";
-import{DeletItem ,OpenAddText,OpenAndCloseText,CloseAddText,TextArea} from "./JSXworking"
+import{DeletItem ,OpenAddText,OpenAndCloseText,CloseAddText,TextArea,Text} from "./JSXworking"
 import { useState } from "react";
 import { useEffect } from "react";
 function List(props) {
 let [openAndCloseTextarea,setOpenAndCloseTextarea]=useState({})
+let [openAndCloseText,setOpenAndCloseText]=useState({})
 let [text,setText]=useState({})
+let[textForEdit,setTextForEdit]=useState("")
 useEffect(() => {
   const initialState = {};
   props.arryObj.forEach((item) => {
     initialState[item.id] = false;
   });
   setOpenAndCloseTextarea(initialState);
-}, [props.arryObj]);
+   setOpenAndCloseText(initialState)
+}, []);
 useEffect(()=>{
   const initialState = {};
   props.arryObj.forEach((item) => {
@@ -21,28 +24,45 @@ useEffect(()=>{
 },[])
   const HandlerOpenAndCloseTextarea=(id)=>{
     setOpenAndCloseTextarea((prev)=>({...prev,[id]:!prev[id]}))
+    setTextForEdit("")
+  }
+  const HandlerOpenAndCloseText=(id)=>{
+    setOpenAndCloseText((prev)=>({...prev,[id]:!prev[id]}))
+
   }
   const GetText =(event)=>{
-    console.log(event.target.value,event.target.id)
   setText((prev)=>({...prev,[event.target.id]:event.target.value}))
   }
  const HandlerText=(id)=>{
- console.log(id,text)
- }
+     setOpenAndCloseText((prev)=>({...prev,[id]:!prev[id]}))
+     setOpenAndCloseTextarea((prev)=>({...prev,[id]:!prev[id]}))
+     setTextForEdit("")
+}
+const OnchangForEdite =(event)=>{
+setTextForEdit(event.target.value)
+}
+const GetTextForEdite=(id)=>{
+const targetObj=props.arryObj.find((obj)=>obj.id==id)
+const TextTarget =targetObj.Text
+setOpenAndCloseTextarea((prev)=>({...prev,[id]:!prev[id]}))
+setTextForEdit(TextTarget)
+setOpenAndCloseText((prev)=>({...prev,[id]:!prev[id]}))
+}
   return (
     <ul className="List">
       {props.arryObj.map((item) => (
         <div key={item.id}>
         <li>
           {item.name}
-          {item.Text?<OpenAndCloseText  />:(openAndCloseTextarea[item.id] ?<CloseAddText handlerOpenAndCloseTextarea={()=>{HandlerOpenAndCloseTextarea(item.id)}}/>:<OpenAddText handlerOpenAndCloseTextarea={()=>{HandlerOpenAndCloseTextarea(item.id)}}/>)}
+          {item.Text?<OpenAndCloseText  openAndCloseText={openAndCloseText[item.id]} handlerOpenAndCloseText={()=>HandlerOpenAndCloseText(item.id)}/>:(openAndCloseTextarea[item.id] ?<CloseAddText handlerOpenAndCloseTextarea={()=>{HandlerOpenAndCloseTextarea(item.id)}}/>:<OpenAddText handlerOpenAndCloseTextarea={()=>{HandlerOpenAndCloseTextarea(item.id)}}/>)}
           <DeletItem
             removeElement={() => {
               props.removElement(item.id);
             }}
           />
         </li>
-        {(openAndCloseTextarea[item.id]&&<TextArea id={item.id} getText={GetText} handlerText={HandlerText} updateRipository={()=>props.updateRipository(item.id,text)}/>)}
+        {(item.Text&&<Text getText={GetText} text={item.Text} openAndCloseText={openAndCloseText[item.id]} getTextForEdite={()=>GetTextForEdite(item.id)} updateRipository={()=>props.updateRipository(item.id,"")}/>)}
+        {(openAndCloseTextarea[item.id]&&<TextArea onchangForEdite={OnchangForEdite} value={textForEdit} id={item.id} getText={GetText} handlerText={()=>{HandlerText(item.id)}} updateRipository={()=>props.updateRipository(item.id,text[item.id])}/>)}
         </div>
       ))}
     </ul>
